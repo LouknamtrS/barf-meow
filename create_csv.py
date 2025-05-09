c08197db6237ed2220d736204e62f984f325e111
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2025-05-09 10:19:47 +0700

    re model

--- create_csv.py
+++ create_csv.py
@@ -5,99 +5,44 @@ import os
 import numpy as np 
 
 def image_processed(file_path):
-    
-    # reading the static image
     hand_img = cv2.imread(file_path)
-
-    # Image processing
-    # 1. Convert BGR to RGB
     img_rgb = cv2.cvtColor(hand_img, cv2.COLOR_BGR2RGB)
 
-    # 2. Flip the img in Y-axis
-    img_flip = cv2.flip(img_rgb, 1)
-
-    # accessing MediaPipe solutions
     mp_hands = mp.solutions.hands
-
-    # Initialize Hands
-    hands = mp_hands.Hands(static_image_mode=True,
-    max_num_hands=1, min_detection_confidence=0.7)
-
-    # Results
+    hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.7)
     output = hands.process(img_rgb)
-
     hands.close()
 
     try:
         data = output.multi_hand_landmarks[0]
-        print(data)
-
-        data = str(data)
-
-        data = data.strip().split('\n')
-
-        garbage = ['landmark {', '  visibility: 0.0', '  presence: 0.0', '}']
-
-        without_garbage = []
-
-        for i in data:
-            if i not in garbage:
-                without_garbage.append(i)
-
-        clean = []
-
-        for i in without_garbage:
-            i = i.strip()
-            clean.append(i[2:])
-
-        for i in range(0, len(clean)):
-            clean[i] = float(clean[i])
-
-        return(clean)
-
+        landmarks = []
+        for lm in data.landmark:
+            landmarks.extend([lm.x, lm.y, lm.z])
+        return landmarks
     except:
-        return(np.zeros([1,63], dtype=int)[0])
+        return np.zeros(63).tolist()
 
 def make_csv():
-    
     mypath = 'DATASET'
-    file_name = open('dataset.csv', 'a')
+    with open('dataset.csv', 'w') as file:
+        headers = [f'x{i}' for i in range(21)] + [f'y{i}' for i in range(21)] + [f'z{i}' for i in range(21)] + ['label']
+        file.write(','.join(headers) + '\n')
 
-    for each_folder in os.listdir(mypath):
-        if '._' in each_folder:
-            pass
+        for each_folder in os.listdir(mypath):
+            if each_folder.startswith('._'):
+                continue
 
-        else:
-            for each_number in os.listdir(mypath + '/' + each_folder):
-                if '._' in each_number:
-                    pass
-                
-                else:
-                    label = each_folder
+            for each_file in os.listdir(os.path.join(mypath, each_folder)):
+                if each_file.startswith('._'):
+                    continue
 
-                    file_loc = mypath + '/' + each_folder + '/' + each_number
+                file_path = os.path.join(mypath, each_folder, each_file)
+                label = each_folder
+                data = image_processed(file_path)
 
-                    data = image_processed(file_loc)
-                    try:
-                        for id,i in enumerate(data):
-                            if id == 0:
-                                print(i)
-                            
-                            file_name.write(str(i))
-                            file_name.write(',')
+                file.write(','.join([str(i) for i in data]) + f',{label}\n')
 
-                        file_name.write(label)
-                        file_name.write('\n')
-                    
-                    except:
-                        file_name.write('0')
-                        file_name.write(',')
-
-                        file_name.write('None')
-                        file_name.write('\n')
-       
-    file_name.close()
-    print('Data Created !!!')
+    print('Data Created!')
 
 if __name__ == "__main__":
     make_csv()

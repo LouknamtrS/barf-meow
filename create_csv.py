f964575969e4fc358b06b05e0d4016c9cbe10b8b
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2026-06-26 15:51:59 +0700

    update readme

--- create_csv.py
+++ create_csv.py
@@ -28,8 +28,8 @@ def image_processed(file_path):
 
 
 def make_csv():
-    mypath = 'DATASET3_augmented'
-    with open('dataset4_augmented.csv', 'w') as file:
+    mypath = 'DATASET_augmented'
+    with open('dataset_augmented.csv', 'w') as file:
         headers = [f'x{i}' for i in range(21)] + [f'y{i}' for i in range(21)] + [f'z{i}' for i in range(21)] + ['label']
         file.write(','.join(headers) + '\n')
 
--- create_dataset.py
+++ create_dataset.py
@@ -3,8 +3,19 @@ import cv2 as cv
 from pathlib import Path
 
 def get_image():
-    Class = 'grab'
-    save_path = Path(f'DATASET3/{Class}')
+    #Class = 'right'
+    #Class = 'left'
+    #Class = 'grab'
+    #Class = 'cooking'
+    #Class = 'stop'
+    #Class = 'recipe'
+    #Class = 'up'
+    #Class = 'down'
+    #Class = 'tips'
+    #Class = 'ok'
+    #Class = 'setting'
+
+    save_path = Path(f'DATASET_NEW/{Class}')
     save_path.mkdir(parents=True, exist_ok=True)
 
     existing_files = list(save_path.glob('*.png'))

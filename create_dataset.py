7651ab60e1b733c6de25800ee5ecc19fac3f11e6
Saete-373 <111549037+Saete-373@users.noreply.github.com>
2026-06-29 18:19:55 +0700

    add new gesture for left hand

--- create_dataset.py
+++ create_dataset.py
@@ -3,18 +3,20 @@ import cv2 as cv
 from pathlib import Path
 
 def get_image():
-    #Class = 'right'
-    #Class = 'left'
-    #Class = 'grab'
-    #Class = 'cooking'
-    #Class = 'stop'
-    #Class = 'recipe'
-    #Class = 'up'
-    #Class = 'down'
-    #Class = 'tips'
-    #Class = 'ok'
-    #Class = 'setting'
-    #Class = 'transition'
+    # Class = 'rh_right'
+    # Class = 'rh_left'
+    # Class = 'lh_right'
+    # Class = 'lh_left'
+    # Class = 'grab'
+    # Class = 'cooking'
+    # Class = 'stop'
+    # Class = 'recipe'
+    # Class = 'up'
+    # Class = 'down'
+    # Class = 'tips'
+    # Class = 'ok'
+    # Class = 'setting'
+    # Class = 'transition'
 
     save_path = Path(f'DATASET_NEW/{Class}')
     save_path.mkdir(parents=True, exist_ok=True)
